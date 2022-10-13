6122c2b4b63231e2e93dc2f8a729f9a5d065a65e
HD <hejunqiang2014@outlook.com>
2022-10-13 17:18:05 +0800

    替换testHomVC

---
+++ DoveOC/DoveOC/Test/HDTestHomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HDTestHomeViewController.h
+//  DoveOC
+//
+//  Created by DOVE on 2022/10/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HDTestHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DoveOC/DoveOC/Test/HDTestHomeViewController.m
@@ -0,0 +1,24 @@
+//
+//  HDTestHomeViewController.m
+//  DoveOC
+//
+//  Created by DOVE on 2022/10/13.
+//
+
+#import "HDTestHomeViewController.h"
+
+@interface HDTestHomeViewController ()
+
+@end
+
+@implementation HDTestHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+}
+
+
+@end
