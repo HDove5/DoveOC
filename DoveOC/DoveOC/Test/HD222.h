1f54760c4ccfc11d16dacf84b419865f8c016cf5
hjq <1452732257@qq.com>
2023-01-05 18:41:05 +0800

    Revert "暂时隐蔽222 - 2"
    This reverts commit fd7bbb1a5309114237a74c607d3d4ffc9fe7679a.

---
+++ DoveOC/DoveOC/Test/HD222.h
@@ -0,0 +1,16 @@
+//
+//  HD222.h
+//  DoveOC
+//
+//  Created by DOVE on 2023/1/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HD222 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DoveOC/DoveOC/Test/HD222.m
@@ -0,0 +1,12 @@
+//
+//  HD222.m
+//  DoveOC
+//
+//  Created by DOVE on 2023/1/5.
+//
+
+#import "HD222.h"
+
+@implementation HD222
+
+@end
